cb4531f26ccad0830da2e8fd0ecf503f930dc097
marcelotejada76 <marcelotejada@gmail.com>
2023-08-15 12:48:30 -0500

    cambio

--- Garantias/ConexionAs400.cs
+++ Garantias/ConexionAs400.cs
@@ -8,6 +8,7 @@ namespace Garantias
 {
     class ConexionAs400
     {
+        //conecta
         public static string Conexion = "DataSource=172.20.16.163;UserID=DGACCONEXI;Password=DGACTIC20@;Database=S10a1a05;DataCompression=True;Default Collection=DGACDAT;";
 
     }
